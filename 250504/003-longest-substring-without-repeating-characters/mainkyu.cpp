56add2cd8a4147f468e945d1a1125fe982b4e16d
Minkyu Lee <whizkyu@snu.ac.kr>
2025-05-04 15:42:46 +0900

    minkyu: solve 003 (2)

--- 250504/003-longest-substring-without-repeating-characters/mainkyu.cpp
+++ 250504/003-longest-substring-without-repeating-characters/mainkyu.cpp
@@ -5,30 +5,25 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        // m[c] = i
-        // i is the index of c in the string s
-        unordered_map<char, int> m;
+        // index on last visit
+        // use fixed-length array because each character of s is ascii
+        int pos[128];
+        memset(pos, -1, sizeof(pos));
 
         int l = -1;
         int max_len = 0;
         for (int i = 0; i < s.size(); ++i) {
             char& c = s[i];
-
-            auto match = m.find(c);
-            if (match != m.end()) {
-                int new_l = match->second;
-                while (l < new_l) {
-                    m.erase(m.find(s[++l]));
-                }
-            }
-            m[c] = i;
-
-            max_len = max(i - l, max_len);
+            l = max(l, pos[c]);
+            pos[c] = i;
+            max_len = max(max_len, i - l);
         }
+        
+        max_len = max(max_len, (int) s.size() - l - 1);
         return max_len;
     }
 };
 
 int main() {
-    println("{}", Solution().lengthOfLongestSubstring("abcabcbb"));
+    println("{}", Solution().lengthOfLongestSubstring("cdmyhietzgalfkjindktzztkpcfghxblaqjjypezweldqwmgcyzbytnn"));
 }
