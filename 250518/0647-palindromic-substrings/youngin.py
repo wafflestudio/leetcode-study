a6f1aec64013eb2e24efe39a99fd9e845fc8e57e
sstto <youngin.sim@navercorp.com>
2025-05-18 11:00:58 +0900

    youngin: solve 0647

--- 250518/0647-palindromic-substrings/youngin.py
+++ 250518/0647-palindromic-substrings/youngin.py
@@ -1,2 +1,23 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
+        n = len(s)
+        palindrome = [[False for _ in range(len(s))] for _ in range(n)]
+        answer = 0
+        for i in range(n):
+            palindrome[i][i] = True
+            answer += 1
+        
+        for i in range(n - 1):
+            if s[i] == s[i + 1]:
+                palindrome[i][i + 1] = True
+                answer += 1
+            else:
+                palindrome[i][i + 1] = False
+
+        for size in range(2, n):
+            for i in range(n - size):
+                if s[i] == s[i + size] and palindrome[i + 1][i + size - 1]:
+                    palindrome[i][i + size] = True
+                    answer += 1
+
+        return answer        
