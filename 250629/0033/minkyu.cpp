89709b67938c10f0756aedcba2b9e9616cea03e4
Minkyu Lee <whizkyu@snu.ac.kr>
2025-06-20 07:21:30 +0900

    minkyu: solve 33

---
+++ 250629/0033/minkyu.cpp
@@ -0,0 +1,13 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); ++i) {
+            if (target == nums[i]) return i;
+        }
+        return -1;
+    }
+};
