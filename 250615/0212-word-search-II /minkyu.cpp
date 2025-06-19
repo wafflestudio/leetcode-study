fb855d0fb858ee1e424907f1b694bf96d802ea6a
Minkyu Lee <whizkyu@snu.ac.kr>
2025-06-20 06:47:29 +0900

    minkyu: solve 212

--- 250615/0212-word-search-II /minkyu.cpp
+++ 250615/0212-word-search-II /minkyu.cpp
@@ -2,14 +2,14 @@
 
 using namespace std;
 
-class Node {
+class TrieNode {
 public:
     bool is_word;
     bool visited;
     string word;
-    unordered_map<char, Node*> children;
+    unordered_map<char, TrieNode*> children;
 
-    Node()
+    TrieNode()
         : is_word(false)
         , visited(false)
         , word("")
@@ -18,33 +18,42 @@ public:
 
 class Solution {
 public:
-    void collect(vector<vector<char>>& board, Node* prev, int x, int y, vector<string>& cum, int m, int n, set<pair<int, int>> visited) {
-        if (x < 0 || x >= m || y < 0 || y >= n || visited.contains({x, y})) return;
-        visited.emplace(x, y);
+    // Improvement 1: It is essential to specify every non-primitive arguments as a reference variable, because the copy operation of huge variable costs really expensive.
+    void collect(vector<vector<char>>& board, TrieNode*& prev, int x, int y, vector<string>& cum, int m, int n, vector<vector<bool>>& visited) {
+        if (x < 0 || x >= m || y < 0 || y >= n || visited[x][y]) return;
+        visited[x][y] = true;
         if (prev->children.contains(board[x][y])) {
-            Node* now = prev->children[board[x][y]];
+            TrieNode* now = prev->children[board[x][y]];
+
             if (now->is_word && !now->visited) {
                 cum.push_back(now->word);
                 now->visited = true;
             }
-            collect(board, now, x+1, y, cum, m, n, visited);
-            collect(board, now, x-1, y, cum, m, n, visited);
-            collect(board, now, x, y+1, cum, m, n, visited);
-            collect(board, now, x, y-1, cum, m, n, visited);
+
+            collect(board, now, x + 1, y, cum, m, n, visited);
+            collect(board, now, x - 1, y, cum, m, n, visited);
+            collect(board, now, x, y + 1, cum, m, n, visited);
+            collect(board, now, x, y - 1, cum, m, n, visited);
+
+            // Improvement 2: If the word is counted and this is a leaf node, remove the current node from the parent node.
+            if (now->is_word && now->visited && now->children.empty()) {
+                prev->children.erase(prev->children.find(board[x][y]));
+            }
         }
+        visited[x][y] = false;
     }
 
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
         int m = board.size();
         int n = board[0].size();
 
-        Node* word_tree = new Node();
+        TrieNode* word_trie = new TrieNode();
 
         for (string& word : words) {
-            Node* now = word_tree;
+            TrieNode* now = word_trie;
             for (char& c : word) {
-                if (!now->children.contains(c)){
-                    now->children[c] = new Node();
+                if (!now->children.contains(c)) {
+                    now->children[c] = new TrieNode();
                 }
                 now = now->children[c];
             }
@@ -53,11 +62,10 @@ public:
         }
 
         vector<string> answer;
-        set<pair<int, int>> visited;
+        vector<vector<bool>> visited(m, vector(n, false));
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                visited.clear();
-                collect(board, word_tree, i, j, answer, m, n, visited);
+                collect(board, word_trie, i, j, answer, m, n, visited);
             }
         }
         return answer;
