dc2288c1904c80cc65de914e9aa46566dc7d56b9
Minkyu Lee <whizkyu@snu.ac.kr>
2025-07-03 15:08:07 +0900

    minkyu: improve 424 (memorize only max count)

--- 250713/0424/minkyu.cpp
+++ 250713/0424/minkyu.cpp
@@ -5,33 +5,13 @@ using namespace std;
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int answer = 0;
         int left = 0;
         int right = 0;
-        vector<bool> visited(128, false);
-        vector<char> candiates;
         vector<int> count_of(128, 0);
+        int max_count = 0;
         for (char& c: s) {
-            char r_char = s[right++];
-            if (!visited[r_char]) {
-                visited[r_char] = true;
-                candiates.push_back(r_char);
-            }
-            int temp = ++count_of[r_char] + k;
-            int q_len = right - left;
-            bool left_step_forward = true;
-            if (temp < q_len) {
-                for (char& candi: candiates) {
-                    int temp = count_of[candi] + k;
-                    if (temp >= q_len) {
-                        left_step_forward = false;
-                        break;
-                    }
-                }
-            } else {
-                left_step_forward = false;
-            }
-            if (left_step_forward) {
+            max_count = max(max_count, ++count_of[s[right++]]);
+            if (max_count + k < right - left) {
                 count_of[s[left++]]--;
             }
         }
@@ -40,7 +20,7 @@ public:
 };
 
 int main() {
-    string s = "BAAAB";
-    int k = 2;
+    string s = "AABABBA";
+    int k = 1;
     println("{}", Solution().characterReplacement(s, k));
 }
